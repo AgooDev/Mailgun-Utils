2bf0add0a1edbda58f3c154772203a631bd6aff8
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-04-27 12:17:55 -0500

    minor text fixed

--- config/config.php
+++ config/config.php
@@ -1,4 +1,4 @@
 <?php
 
-define('MAILGUN_APIKEY', '1234567890abc');
+define('MAILGUN_PRIVATE_APIKEY', '1234567890abc');
 define('MAILGUN_DOMAIN', 'myDomain.com');
--- viaAPI.php
+++ viaAPI.php
@@ -1,1 +1,27 @@
 <?php
+# Include autoloader
+require_once 'config/config.php';
+require 'vendor/autoload.php';
+use Mailgun\Mailgun;
+
+# Email Variables
+$from = '';
+$text = '';
+$to = '';
+$subject = '';
+$text = '';
+$html = '';
+$recipientVariables = '';
+$from = '';
+$from = '';
+$from = '';
+
+
+# Instantiate the client
+$messageClient = new Mailgun(MAILGUN_APIKEY);
+
+# Now, compose the mail and call the client to send it
+$messageClient->sendMessage(MAILGUN_DOMAIN,array(
+    'from'  =>  $from,
+    
+));
