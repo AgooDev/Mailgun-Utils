c90d84295a07e00e92a5a988f8562e660cc52852
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-04-27 13:12:27 -0500

    general stucture to send email via API of mailgun

--- viaAPI.php
+++ viaAPI.php
@@ -5,23 +5,36 @@ require 'vendor/autoload.php';
 use Mailgun\Mailgun;
 
 # Email Variables
+# postData
 $from = '';
 $text = '';
 $to = '';
+$cc = '';
+$bcc = '';
 $subject = '';
 $text = '';
 $html = '';
 $recipientVariables = '';
-$from = '';
-$from = '';
-$from = '';
+
+# postFiles
+$attachment = array('/path/to/file.txt','/the/second/file/to/attach.pdf');
+$images = '';
 
 
 # Instantiate the client
-$messageClient = new Mailgun(MAILGUN_APIKEY);
+$messageClient = new Mailgun(MAILGUN_PRIVATE_APIKEY);
 
 # Now, compose the mail and call the client to send it
 $messageClient->sendMessage(MAILGUN_DOMAIN,array(
-    'from'  =>  $from,
-    
+    'from'                  =>  $from,
+    'to'                    =>  $to,
+    'cc'                    =>  $cc,
+    'bcc'                   =>  $bcc,
+    'subject'               =>  $subject,
+    'text'                  =>  $text,
+    'html'                  =>  $html,
+    'recipient-variables'   =>  '{}'
+), array(
+    'attachment'    =>  $attachment,
+
 ));
