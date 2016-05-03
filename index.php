8f01ca39fd36ac2e87a58b79a782850bb4f97b55
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-05-03 18:06:02 -0500

    adding slim to index pag

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
 <?php
+require 'vendor/autoload.php';
+
+$app = new \Slim\App();
+
+$app->get('/', function ($request, $response, $args) {
+    return $response->withStatus(200)->write('Service Available');
+});
+
+// TEMPLATES FUNCTIONS
+// ================================================================
+// WELCOME
+
+$app->get('/welcome', function ($request, $response, $args) {
+    $response->getBody()->write("Welcome");
+    return $response;
+});
+
+
+
+$app->run();
