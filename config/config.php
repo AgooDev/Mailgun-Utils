13ed7748aee9fdecf14157e0da287311b58efb5a
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-05-03 18:05:35 -0500

    db configs

--- config/config.php
+++ config/config.php
@@ -2,3 +2,7 @@
 
 define('MAILGUN_PRIVATE_APIKEY', '1234567890abc');
 define('MAILGUN_DOMAIN', 'myDomain.com');
+define('DB_CONNECTION','myconn.server.com:port');
+define('DB_NAME','dbName');
+define('DB_USER','dbUser');
+define('DB_PASSWORD','dbPass');
