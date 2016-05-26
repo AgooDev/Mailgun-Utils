3b23c3b38c16851408a9688f93f5ba88caccd53c
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-05-25 23:44:45 -0500

    site root constant defined

--- config/config.php
+++ config/config.php
@@ -6,4 +6,5 @@ define('DB_CONNECTION','myconn.server.com:port');
 define('DB_NAME','dbName');
 define('DB_USER','dbUser');
 define('DB_PASSWORD','dbPass');
+define('SITE_ROOT', '/var/www/html/mailgun');
 define('HISTORY_PATH','assets/history/');
