e7942c8fa911bb5918c686faf4194eb00b1436a6
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-05-26 01:46:21 -0500

    avoid free methods

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Utilidades para Usar Mailgun en Agoo.com.co (Mailgun-Utils)
 
 Este repositorio contiene una compilación de snippet, casos de uso y codigo prefabricado.
+
+/welcome
+
+```
+{
+    "from":"noreplay@domain.com",
+    "to":"user@email.com",
+    "subject":"welcome",
+    "name":"user name",
+    "password":"1234"
+}
+```
+
+/ticket
+
+/recovery
--- index.php
+++ index.php
@@ -138,55 +138,6 @@ $app->post('/ticket', function ($request, $response){
     die();
 });
 
-
-// ================================================================
-// FREE
-$app->get('/free', function ($request, $response, $args) {
-    $response->getBody()->write("Free");
-    return $response;
-});
-
-$app->post('/free', function ($request, $response){
-    # Get body data
-    $data = $request->getParsedBody();
-    # Instantiate the client
-    $client = new \Http\Adapter\Guzzle6\Client();
-    $messageClient = new \Mailgun\Mailgun(MAILGUN_PRIVATE_APIKEY, $client);
-
-    # Email Variables
-    # postData
-    $from       = filter_var($data['from'],     FILTER_SANITIZE_EMAIL);
-    $to         = filter_var($data['to'],       FILTER_SANITIZE_EMAIL);
-    $subject    = filter_var($data['subject'],  FILTER_SANITIZE_STRING);
-    $name       = filter_var($data['name'],     FILTER_SANITIZE_STRING);
-    $password   = filter_var($data['password'], FILTER_SANITIZE_STRING);
-
-    # Create html file to send
-    $templates  = new HTML();
-    $data = array(
-        'name'      => $name,
-        'email'     => $to,
-        'password'  => $password,
-    );
-    $templateRoute = $templates->CreateTemplate("free", $data);
-    # Get the file in memory to attached
-    $html = file_get_contents($templateRoute);
-
-    # Now, compose the mail and call the client to send it
-    $result = $messageClient->sendMessage(MAILGUN_DOMAIN,array(
-        'from'                  =>  "Agoo.com.co <".$from.">",
-        'to'                    =>  $to,
-        'subject'               =>  $subject,
-        'html'                  =>  $html
-    ));
-
-    # Results
-    $httpResponseCode = $result->http_response_code;
-    $httpResponseBody = $result->http_response_body;
-
-    echo $from,$to,$subject,$name,$password, $templateRoute, " codigo: ", $httpResponseCode, ", mensaje: ", $httpResponseBody;
-    die();
-});
 // ================================================================
 // RECOVERY
 $app->get('/recovery', function ($request, $response, $args) {
