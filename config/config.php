827f2ee22db5745c4c130d61b0c6954640cadef5
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-06-02 15:51:08 -0500

    config update

--- config/config.php
+++ config/config.php
@@ -6,5 +6,12 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree or translated in the assets folder.
  */
-define('SITE_ROOT', '/var/www/html/mailgun');
-define('HISTORY_PATH','assets/history/');define('TICKET_ROOT', '/var/www/tickets');
+define('MAILGUN_PRIVATE_APIKEY', '1234567890abc');
+define('MAILGUN_DOMAIN', 'myDomain.com');
+define('DB_CONNECTION','myconn.server.com:port');
+define('DB_NAME','dbName');
+define('DB_USER','dbUser');
+define('DB_PASSWORD','dbPass');
+define('SITE_ROOT', '/var/www/html/Mailgun-Utils');
+define('HISTORY_PATH','assets/history/');
+define('TICKET_ROOT', '/var/www/html/tickets');
